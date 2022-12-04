41a5ade7150b43e1a61400f0a92d514247946364
Diego Tobar <dyegote26@gmail.com>
2022-12-04 16:50:02 -0500

    Add custom icon marker

--- app/src/main/java/com/diegot/fletx/maps/MapActivity.kt
+++ app/src/main/java/com/diegot/fletx/maps/MapActivity.kt
@@ -1,13 +1,19 @@
 package com.diegot.fletx.maps
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.diegot.fletx.R
 import com.diegot.fletx.uimodel.VehicleUiModel
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -40,11 +46,22 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         vehicleModel?.let {
             vehicleMap.addMarker(
                 MarkerOptions()
+                    .icon(bitmapDescriptorFromVector(this, R.drawable.ic_vehicle_marker))
                     .position(it.location)
-                    .title(it.placa))
+                    .title(it.placa)
+            )
             vehicleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(it.location, 10f))
             vehicleMap.setInfoWindowAdapter(MapMarkerAdapter(this, it)
             )
+        }
+    }
+
+    private fun bitmapDescriptorFromVector(context: Context, vectorResId: Int): BitmapDescriptor? {
+        return ContextCompat.getDrawable(context, vectorResId)?.run {
+            setBounds(0, 0, intrinsicWidth, intrinsicHeight)
+            val bitmap = Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888)
+            draw(Canvas(bitmap))
+            BitmapDescriptorFactory.fromBitmap(bitmap)
         }
     }
 }
